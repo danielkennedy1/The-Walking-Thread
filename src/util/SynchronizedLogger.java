8793b31419e2f7c54d0ac89ba13c6d9c43a4dce9
JohnFoley <johnmfoley2003@yahoo.com>
2023-05-18 16:37:37 +0100

    added necessary comments to synchronizedLogger, trivia API and quotes API

--- src/util/SynchronizedLogger.java
+++ src/util/SynchronizedLogger.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 
 public class SynchronizedLogger implements Runnable{
 
+    //attributes we want to be passed into constructor, which will be logged
     private Socket clientSocket;
     private Message messageIn;
     private String responseContent;
-    private String clientName;
 
     public SynchronizedLogger(Socket clientSocket, Message messageIn, String responseContent){
         this.clientSocket = clientSocket ;
@@ -17,14 +17,21 @@ public class SynchronizedLogger implements Runnable{
         this.responseContent = responseContent;
     }
 
+    // synchronized method run so that only one thread can write to the logger.txt file at a time
     @Override
     public synchronized void run() {
         try {
+            // create a file writer which will write to logger.txt
             FileWriter writer = new FileWriter("src/util/logger.txt", true);
+            // convert local socket address of client to a string
             String stringSocketAddress = String.valueOf(clientSocket.getLocalSocketAddress());
+            // find time when log is happening
             LocalDateTime logTime = LocalDateTime.now();
             String logString = String.valueOf(logTime);
             String text;
+            // check if the message in is a chat or a command
+            // if the message is a chat, we want to return the sender name
+            // if the message is a command, we want to return the command
             switch (messageIn.getClass().getSimpleName()) {
                 case "Chat":
                     String sender = ((Chat) messageIn).getSenderName();
--- src/util/TriviaAPI.java
+++ src/util/TriviaAPI.java
@@ -8,6 +8,10 @@ import java.net.URL;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+// This class contains three methods. getTriviaQuestion, getQuestion and getCorrectAnswer
+// getTriviaQuestion: calls to api, receives a json string parses it and extracts the question and answer
+// getQuestion and getCorrectAnswer return a question and answer
+// Note: we have filtered the API so that it returns computer science related questions
 public class TriviaAPI {
 
     private static String question = "";
@@ -15,15 +19,19 @@ public class TriviaAPI {
 
 
     public static void getTriviaQuestion() {
+        // URL link to API
         String apiUrl = "https://opentdb.com/api.php?amount=1&category=18&difficulty=medium";
         StringBuilder response = new StringBuilder();
 
+        // try to establish connection to the URL and send a GET request
         try {
             URL url = new URL(apiUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
+            // get response code to verify content was successfully received
             int responseCode = connection.getResponseCode();
+            // if response code is ok, then read the response in
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String line;
@@ -33,16 +41,20 @@ public class TriviaAPI {
                 }
                 reader.close();
             }
+            // close connection when finished reading
             connection.disconnect();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // using regex, create a pattern to look for in the response
         Pattern questionPattern = Pattern.compile("\"question\":\"([^\"]+)\"" );
+        // check for regex matches between pattern and response
         Matcher questionMatcher = questionPattern.matcher(response);
         if (questionMatcher.find()) {
             question = questionMatcher.group(1);
         }
 
+        // repeat the step above, but this time for the correct answer instead of the question
         Pattern answerPattern = Pattern.compile("\"correct_answer\":\"([^\"]+)\"" );
         Matcher answerMatcher = answerPattern.matcher(response);
         if (answerMatcher.find()) {
